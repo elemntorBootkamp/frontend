c0d79383c754a075bdedb0a084c6ca6e40a61dc1
MichalRevivo <michal0527660276@gmail.com>
2023-05-21 15:30:41 +0300

    Update first.jsx

--- Elementor/src/components/first.jsx
+++ Elementor/src/components/first.jsx
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
  export const User=()=>{
     
     
     return<>
-=======
-export const User=()=>{
->>>>>>> 5b644eb32657a533c29b65cc34ee5bfa67ece79c
     <h1>hello girl!</h1>
     <h2>hello 😊😂🤣😁😘😒</h2>
     </>
