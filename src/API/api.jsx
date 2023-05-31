452e5c4fdb354ab33089c4fa9a0a7d02e8ad58f5
shifraDomb <do8259@gmail.com>
2023-05-31 10:16:03 +0300

    importantChange

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -3,10 +3,10 @@ import axios from 'axios'
 //פונקציית GET כרגע לסתם שרת!!
 //מקבלת משתמש מחפשת אותו ומחזירה את כל הפרטים שלו
 //יכול להיות שבמבנה צריך לקבל גם את שם הפונקציה בתור משתנה 
-export const getAll=async(user)=>{
+export const getAll=async()=>{
     try
     {
-        const response=await axios.get(`${process.env.port}/`)
+        const response=await axios.get(``)
         return response.data
     }
     
