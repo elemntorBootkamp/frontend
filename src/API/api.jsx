7b67226c851c454528361a690edfab91fb244433
dvora <dvorat377@gmail.com>
2023-05-31 13:06:32 +0300

    Rina

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -3,10 +3,10 @@ import axios from "axios"
 //פונקציית GET כרגע לסתם שרת!!
 //מקבלת משתמש מחפשת אותו ומחזירה את כל הפרטים שלו
 //יכול להיות שבמבנה צריך לקבל גם את שם הפונקציה בתור משתנה 
-export const getById=async(user)=>{
+export const getAll=async()=>{
     try
     {
-        const response=await axios.get(`http://localhost:3001/userRouter/${user}`)
+        const response=await axios.get(`http://localhost:8080/getAll`)
         return response.data
     }
     catch(err)
--- src/Redux/myStore.jsx
+++ src/Redux/myStore.jsx
@@ -1,14 +1,10 @@
 import { createStore, combineReducers } from "redux";
-//יכולת של ריקס לבנות סטור- מחסן
-import { createStore} from "redux";
-//הכרות עם הרדוסר הרלוונטי
-//הנתונים הגלובאליים הרלוונטיים
 import {dataReducer } from "./dataReducer";
 
 
 const reducers =dataReducer ;
-  
- export const ourStore = createStore(reducers)
+ 
+export const ourStore = createStore(reducers)
 
  window.store=ourStore;
 
