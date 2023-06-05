320269ba197a3b4237bcf76f9401ea5176ab9f56
ayalaBiton <ayala59924@gmail.com>
2023-06-05 15:50:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-// import { useState } from 'react'
+/* eslint-disable linebreak-style */
+// import { useState } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { Provider } from 'react-redux';
 import './App.css';
-// import {UpdateManagerDatails} from "./components/login"
-import { ShowAllCaterogies } from './components/login';
-// import {ShowAllUsers} from"./components/login"
-
-// import {SmallComp} from"./components/login"
+// import {Botton} from './stories/Button'
 import { ourStore } from './Redux/myStore';
+//import { Page } from './stories/Page'
+//import {Hello} from"./components/login"
+import React from 'react';
+
+import { User } from './components/first';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* store-יש להגדיר ברכיב
-   את המחסן שלנו*/}
-
-      {/* <div>hello</div> */}
-
       <Provider store={ourStore}>
         {/* <Data1></Data1> */}
-        {/* <SmallComp></SmallComp>  */}
-        {/* <ShowAllUsers></ShowAllUsers> */}
-        <ShowAllCaterogies></ShowAllCaterogies>
+
+        {/* <Page></Page> */}
         {/* <UpdateManagerDatails></UpdateManagerDatails>  */}
+
+        {/* <Hello></Hello> */}
+        <User></User>
       </Provider>
     </>
   );
