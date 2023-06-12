c2a228747d3d79f7b5c40ca93e2667e3e88b8010
shifraDomb <do8259@gmail.com>
2023-06-12 13:47:37 +0300

    try

--- cypress/e2e/example.cy.js
+++
@@ -1,7 +0,0 @@
-
-describe("create todo spec", () => {
-    before(() => {
-      // this will launch the page in cypress browser
-      cy.visit("http://localhost:3000");
-    });
-  })
--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,9 +1,10 @@
+
 describe('Signup Component', () => {
     beforeEach(() => {
       cy.visit('http://localhost:3000/signup')
     })
   
-    it('should render the h1 with the correct contebt', () => {
+    it('should render the Signup component correctly', () => {
       cy.contains('signup').should('exist')
     })
   })
