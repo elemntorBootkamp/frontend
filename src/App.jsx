9459306e1ff0b66d33d2bff3862174348b6021ad
MichalRevivo <michal0527660276@gmail.com>
2023-06-12 09:36:45 +0300

    09:36

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import React from 'react';
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from './Keycloak';
 import { Provider } from 'react-redux'
 import './App.css'
 import { ourStore } from './Redux/myStore'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import { Routing } from '../allrouting'
 import { Page } from '../src/components/Page'
-
+// import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from './components/nav';
+import Home from './components/homepage';
+import Secured from './components/Securedpage';
+import PrivateRoute from './helpers/PrivateRoute';
 function App() {
   
 
 	return <>
+
+<div>
+     <ReactKeycloakProvider authClient={keycloak}>
+       <Nav />
+       <BrowserRouter>
+         <Routes>
+           <Route exact path="/" element={<Home />} />
+           <Route
+             path="/secured"
+             element={
+               <PrivateRoute>
+                 <Secured />
+               </PrivateRoute>
+             }
+           />
+         </Routes>
+       </BrowserRouter>
+     </ReactKeycloakProvider>
+   </div>
+
 		{/* store-יש להגדיר ברכיב
    את המחסן שלנו*/}
 		{/* <div>hello</div> */}
-    <Provider store={ourStore}>
+    {/* <Provider store={ourStore}>
     <BrowserRouter>
     <Page></Page>
     <Routing></Routing>
     </BrowserRouter>
-    </Provider>
+    </Provider> */}
   </>
 
 }
