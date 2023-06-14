b35b08342f7be741223065da90460e3202c65cf1
dvora <dvorat377@gmail.com>
2023-06-14 15:22:17 +0300

    finish env

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { ourStore } from './Redux/myStore'
 import { BrowserRouter } from 'react-router-dom'
 import { Routing } from '../allrouting'
 import { Page } from '../src/components/Page'
+import{User} from "./components/first"
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
     <Page></Page>
     <Routing></Routing>
     </BrowserRouter>
+    <User></User>
     </Provider>
   </>
 
