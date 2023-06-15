3dfd4b083995663e900480ecb8138b6ed1047fed
YehudisSternberg <133571815+YehudisSternberg@users.noreply.github.com>
2023-06-15 14:50:07 +0300

    y

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,11 @@ import './App.css'
 import { ourStore } from './Redux/myStore'
 import { BrowserRouter } from 'react-router-dom'
 import { Routing } from '../allrouting'
-import { Page } from '../src/components/Page'
+import { Page } from "../src/components/Page"
 
 function App() {
   
-
 	return <>
-		{/* store-יש להגדיר ברכיב
-   את המחסן שלנו*/}
-		{/* <div>hello</div> */}
     <Provider store={ourStore}>
     <BrowserRouter>
     <Page></Page>
