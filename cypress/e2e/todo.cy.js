a83ccc617782cc3381247045c51ee6f16331de69
ayalaBiton <ayala59924@gmail.com>
2023-06-20 19:43:07 +0300

    20.06 19:43

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,9 +1,12 @@
-describe("create todo spec", () => {
-    before(() => {
-        // this will launch the page in cypress browser
-        cy.visit("http://localhost:3000");
-    });
-})
+// eslint-disable-next-line no-undef
+describe('create todo spec', () => {
+	// eslint-disable-next-line no-undef
+	before(() => {
+		// this will launch the page in cypress browser
+		// eslint-disable-next-line no-undef
+		cy.visit('http://localhost:3000');
+	});
+});
 //npx cypress run
 
 
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,12 +3,12 @@ import { Button } from '../components/Button';
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
 
-  title: 'Button',
-  component: Button,
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+	title: 'Button',
+	component: Button,
+	tags: ['autodocs'],
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
