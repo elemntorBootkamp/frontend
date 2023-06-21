ee628e818c9f8ae332baf336dcb1bf5bad73b95d
ayalaBiton <ayala59924@gmail.com>
2023-06-21 09:53:25 +0300

    try

--- allrouting.jsx
+++ allrouting.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Routes } from 'react-router';
- 
+    
 export const Routing=()=>{
 	// eslint-disable-next-line react/react-in-jsx-scope
 	return <>
 		<Routes>
 		</Routes>
 	</>;
+	
 };
