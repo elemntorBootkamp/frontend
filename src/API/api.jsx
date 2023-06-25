caa4d732c46ae4cadd226f9c93ca9dfa8f17d6a0
YehudisSternberg <133571815+YehudisSternberg@users.noreply.github.com>
2023-06-25 13:23:11 +0300

    j

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -1,20 +1,12 @@
 import axios from 'axios';
 
 export const getAll=async()=>{
-<<<<<<< HEAD
 	const port = import.meta.env.VITE_PORT || 8090;
    
 	try
 	{
 		const response=await axios.get(`http://localhost:${port}/api/getAll`);
 		return response.data;
-=======
-    const port = import.meta.env.VITE_PORT || 8090 
-    try
-    {
-        const response=await axios.get(`http://localhost:${port}/api/getAll`)
-        return response.data
->>>>>>> c7775dea268087aa10f0386bf4b45ef3c23e8e35
         
 	}
 	catch(err)
